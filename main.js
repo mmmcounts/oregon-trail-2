3b4efdfbf93f994e7bf313b9f0bab1510144b08b
Mike <mmmcounts@gmail.com>
2020-07-14 14:47:34 -0500

    oregon trail part 2

--- main.js
+++ main.js
@@ -64,3 +64,40 @@ class Wagon {
         // Returns total amount of food among all passengers in the wagon
     }
 }
+
+class Doctor extends Traveler {
+    constructor(name) {
+        super(name);
+    }
+    heal(traveler) {
+        if (traveler.isHealthy === false) {
+            traveler.isHealthy = true
+        }
+    }
+}
+class Hunter extends Traveler {
+    constructor(name) {
+        super(name);
+        this.food = 2
+    }
+    hunt() {
+        this.food += 5
+    }
+    eat() {
+        if (this.food > 1) {
+            this.food -= 2
+        } else if (this.food === 1) {
+            this.food -= 1
+            this.isHealthy = false
+        } else {
+            this.isHealthy = false
+        }
+    }
+    giveFood(traveler, numOfFoodUnits) {
+
+        if (this.food >= numOfFoodUnits) {
+            this.food -= numOfFoodUnits;
+            traveler.food += numOfFoodUnits
+        }
+    }
+}
